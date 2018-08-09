544d18b3f39fc8eb7d9d40e80d54b6171cb206e5
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-09 20:12:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function add(a,b) {
 function subtract(a,b) {
   return a - b 
 }
+function multiply(a,b) {
+  return a*b
+}
+function divide(a,b) {
+  return a/b
+}
+function inc(n) {
+  n++
+}
+function dec(n) {
+  n--
+}
+function makeInt(n) {
+  
+}
